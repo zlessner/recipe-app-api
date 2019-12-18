685e9a34c25d8a431cba493753c61e440bca5154
zlessner <LessnerZachary@gmail.com>
2019-12-17 21:53:42 -0500

    finding AssertionError

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,29 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    #Inherit from TestCase in parenthasees
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@test.com',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@test.com',
+            password='password123',
+            name='Test user full name'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        #response
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
