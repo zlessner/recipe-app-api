ed7c988e6daa632b88c97a39f3a268fed9ff40ce
zlessner <LessnerZachary@gmail.com>
2019-12-19 10:39:45 -0500

    About to launch db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -9,6 +9,7 @@ class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
+        """Handle the command"""
         self.stdout.write('Waiting for database...')
         db_conn = None
         while not db_conn:
